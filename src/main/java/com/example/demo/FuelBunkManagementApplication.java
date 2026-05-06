1bacf98d42c4ddb429f7a25732dee7847d726f6e
LAPTOP-C3JBG2M9\Shashank <mshashankgoud83308@gmail.com>
2026-05-06 17:41:46 +0530

    intial commit

---
+++ src/main/java/com/example/demo/FuelBunkManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelBunkManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelBunkManagementApplication.class, args);
+	}
+
+}
