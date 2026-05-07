b84a8c6dcc3de4dc0fa0a3aa14bc2059aaf375a4
hemanth sai <madalahemanthsai@gmail.com>
2026-05-07 11:49:34 +0530

    added login.jsp and some code in auth controller and gave db password in properties file

--- src/main/java/com/example/demo/Controllers/AuthController.java
+++ src/main/java/com/example/demo/Controllers/AuthController.java
@@ -1,5 +1,19 @@
 package com.example.demo.Controllers;
 
-public class AuthController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
+public class AuthController {
+	@GetMapping("/")
+	public String test()
+	{
+		return "login";
+	}
+	@PostMapping("/login")
+	public void login()
+	{
+		
+	}
 }
